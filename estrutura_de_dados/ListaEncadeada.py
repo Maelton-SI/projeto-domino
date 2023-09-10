e52088fbded4ec9cec314ce762b08586ab42a627
Analarie <alsgs2@aluno.ifal.edu.br>
2023-09-10 13:53:39 -0300

    ListaEncadeada.size() implementada

--- estrutura_de_dados/ListaEncadeada.py
+++ estrutura_de_dados/ListaEncadeada.py
@@ -3,11 +3,20 @@ from estrutura_de_dados.Noh import Noh
 class ListaEncadeada:
     def __init__(self):
         self.head = None
+        self.size = 0
     
     def is_empty(self):
         return self.head == None
 
     def add(self, item):
         novo_noh = Noh(item)
-        novo_noh.setProximo = self.head
+        novo_noh.setProximo(self.head)
         self.head = novo_noh
+        self.size =+ 1 
+    
+    def __len__(self):
+        return self.size 
+    
+
+
+    
