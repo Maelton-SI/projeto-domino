8a566358d007f7a5fae377338a6ede5e01fb6c21
EMESSON HORACIO DOS SANTOS <2022005052@IFAL-MACEIO.INTRANET>
2023-09-14 17:24:58 -0300

    distribuicao_de_pecas_incompleto

--- main.py
+++ main.py
@@ -1,4 +1,38 @@
 from classes.Domino import Domino
+from funcoes.menus import *
+from classes.Jogador import *
 
 d = Domino()
-d.imprime_domino()
+
+while True:
+    qtd_jogadores = menu_inicial()
+    if qtd_jogadores == 2:
+        jogador1 = Jogador()
+        jogador1.nome = nome_jogador(1)
+        jogador2 = Jogador()
+        jogador2.nome = nome_jogador(2)
+        break
+    elif qtd_jogadores == 3:
+        jogador1 = Jogador()
+        jogador1.nome = nome_jogador(1)
+        jogador2 = Jogador()
+        jogador2.nome = nome_jogador(2)
+        jogador3 = Jogador()
+        jogador3.nome = nome_jogador(3)
+        break
+    elif qtd_jogadores == 4:
+        jogador1 = Jogador()
+        jogador1.nome = nome_jogador(1)
+        jogador2 = Jogador()
+        jogador2.nome = nome_jogador(2)
+        jogador3 = Jogador()
+        jogador3.nome = nome_jogador(3)
+        jogador4 = Jogador()
+        jogador4.nome = nome_jogador(4)
+        break
+    else:
+        print('Opção inválida')
+
+print(jogador1.nome)
+print(jogador2.nome)
+# d.imprime_domino()
